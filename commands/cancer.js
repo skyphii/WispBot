beeec4f174f29e71a8bd32b90acbcef17645d87a
WispGuardian <wispguardian@protonmail.com>
2022-08-23 16:26:13 -0400

    Fixed kole quotes command

--- commands/cancer.js
+++ commands/cancer.js
@@ -2,12 +2,17 @@ const Discord = require("discord.js");
 const data = require("../data.json");
 
 module.exports.run = async (bot, message, args) => {
-  var koleCheck = message.author.id == '247498651859943425';
+  var koleCheck = message.author.id === '247498651859943425';
   if(!koleCheck) return message.reply("Only the corn master may use such a prestigous command.");
 
   const list = data.koleQuotes;
+
   let num = Math.floor(Math.random() * list.length);
-  const quote = statuses[num];
+  if(args.length > 0) {
+    const n = parseInt(args[0]);
+    if(n && !isNaN(n) && n > 0 && n <= list.length) num = n-1;
+  }
+  const quote = list[num];
   message.reply(quote + " **[#" + (num+1) + "]**");
 };
 
